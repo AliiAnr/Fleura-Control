dd39af319ecd572cf143b6b7802e0df637d39851
RamaAlqdri <ramadhanialqadri12@gmail.com>
2025-06-24 17:49:34 +0800

    core(git): update ignore

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,7 @@
 export default function DashboardPage() {
   return (
     <div>
-        <p className="text-xl font-semibold">Dashboard</p>
-
-
+      <p className="text-xl font-semibold">Dashboard</p>
     </div>
   );
 }
