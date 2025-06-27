049627c71291451226deb9544325ee9f9473dfb2
RamaAlqdri <ramadhanialqadri12@gmail.com>
2025-06-28 03:53:35 +0800

    fix build issue

--- app/products/detail/[id]/page.tsx
+++ app/products/detail/[id]/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { Button } from "@/components/tremor/Button";
 import { Card } from "@/components/tremor/Card";
